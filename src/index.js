95035e2fc42344fbc77500bc5661915f8e6ff615
Anderson <mfderson@gmail.com>
2019-11-18 19:14:08 -0300

    Configurando projeto

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import './config/ReactotronConfig';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+console.tron.log('Hello World');
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
