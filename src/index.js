6dc5e3502369fba63e5301369641355f120c4b4f
Anderson <mfderson@gmail.com>
2019-11-18 21:39:04 -0300

    React navigation

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 
 import './config/ReactotronConfig';
+import 'react-native-gesture-handler';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-console.tron.log('Hello World');
+import Routes from './routes';
 
 export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-      </View>
-    );
+    return <Routes />;
   }
 }
